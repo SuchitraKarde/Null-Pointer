c36cb80356a012eb27c42a44d899503a3605d9e3
Suchitra Karde <suchitrakarde23@gmail.com>
2025-08-08 21:46:41 +0530

    Initial commit

---
+++ src/assets/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages/Home";
+import MapPage from "../pages/MapPage";
+import LessonPage from "../pages/LessonPage";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/map" element={<MapPage />} />
+        <Route path="/lesson/:id" element={<LessonPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
